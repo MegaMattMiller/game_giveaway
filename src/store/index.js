1fd073209f1b65360efbdc4642710315d20a46ee
Matt Miller <matt@mattmiller.dev>
2021-01-18 14:33:30 -0600

    feat: built basic app with FaunaDB

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,67 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+var faunadb = require('faunadb'),
+  q = faunadb.query;
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    data: ''
+  },
+  mutations: {
+    updateData(state, value) {
+      state.data = value;
+    }
+  },
+  actions: {
+    getData(context) {
+      return new Promise((resolve, reject) => {
+        var client = new faunadb.Client({
+          secret: process.env.VUE_APP_APP_TOKEN,
+          domain: 'db.fauna.com',
+          scheme: 'https'
+        });
+        var helper = client.paginate(q.Match(q.Index('active')));
+        helper
+          .map(function(ref) {
+            return q.Get(ref);
+          })
+          .each(function(page) {
+            console.dir(page); // Logs the retrieved documents.
+            if (page.length < 1) {
+              reject(page);
+            }
+            var data = {};
+            data.games = [];
+            page.forEach((element) => {
+              if (element.data.active) {
+                data.games.push(element.data);
+              }
+            });
+            context.commit('updateData', data);
+            resolve(data);
+          });
+      });
+    }
+    // getData(context) {
+    //   return new Promise((resolve, reject) => {
+    //     let url = '/games.json';
+    //     fetch(url)
+    //       .then((response) => {
+    //         if (!response.ok) {
+    //           reject();
+    //         }
+    //         response.json().then((data) => {
+    //           context.commit('updateData', data);
+    //           resolve(data);
+    //         });
+    //       })
+    //       .catch((err) => {
+    //         reject(err);
+    //       });
+    //   });
+    // }
+  },
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-overlay :show="loading" rounded="sm" style="min-height: 100vh;">
+    <h1>Matt's Extra Games!</h1>
+    <h2>Let me know which games you redeem!</h2>
+    <b-card v-for="game in this.$store.state.data.games" :key="game.id">
+      <b-card-title>{{ game.title }}</b-card-title>
+      <b-button :href="game.humble_url" variant="primary">I'll take it!</b-button>
+    </b-card>
+  </b-overlay>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      loading: true
+    };
+  },
+  mounted() {
+    this.$store.dispatch('getData').then(() => {
+      this.loading = false;
+    });
   }
 };
 </script>
