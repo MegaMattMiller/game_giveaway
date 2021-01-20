9fafcbcbfd20093d469645bccac5887bedcb907f
Matt Miller <matt@mattmiller.dev>
2021-01-19 18:21:53 -0600

    feat: set up boilerplate add page and auth settings

---
+++ src/views/Add.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-overlay :show="loading" rounded="sm" style="min-height: 100vh;">
+    <h1>Page for adding games!</h1>
+  </b-overlay>
+</template>
+<script>
+export default {
+  name: 'Add',
+  data() {
+    return {
+      loading: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.footer-div {
+  text-align: center;
+}
+
+.footer-logo {
+  text-align: center;
+  font-size: 40px;
+  color: #000;
+}
+
+.details-button {
+  margin-left: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <b-container>
       <h1>Matt's Extra Games!</h1>
       <h2>Let me know which games you redeem!</h2>
-      <b-card v-for="game in this.$store.state.data.games" :key="game.id" border-variant="dark">
+      <b-card v-for="(game, index) in this.$store.state.data.games" :key="index" border-variant="dark">
         <b-card-title>{{ game.title }}</b-card-title>
         <b-button :href="game.humble_url">I'll take it!</b-button>
         <b-button :href="getSearchUrl(game.title)" class="details-button">What is this game?</b-button>
@@ -18,8 +18,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'Home',
   data() {
